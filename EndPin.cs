2ef704438c80e38f8daa227d19203d4370f755f2
Wayne Allan Campbell <wcampbell1995@gmail.com>
2016-02-19 18:39:41 -0500

    Added Test files, included everything in the solution.

--- EndPin.cs
+++ EndPin.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace EndPinLibrary
+namespace EndRectangleLibrary
 {
     public class EndRectangle
     {
---
+++ UnitTestProject1/TestEndPin.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using EndRectangleLibrary;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class TestEndPin
+    {
+        EndRectangle endRectangle = new EndRectangle();
+
+        [TestMethod]
+        public void TestWidthCalculation()
+        {
+            double inputWidth = 10;
+            double clearance = 10;
+            double endMill = .125;
+
+            Assert.AreEqual(20.125,endRectangle.calculateWidth(inputWidth, clearance, endMill));
+        }
+
+        [TestMethod]
+        public void TestHeightCalculation()
+        {
+            double inputThickness = 20;
+            double endMill = .125;
+
+            Assert.AreEqual(20.250, endRectangle.calculateHeight(inputThickness, endMill));
+        }
+
+        [TestMethod]
+        public void TestX1Calculation()
+        {
+            double origin = 0.0;
+
+            Assert.AreEqual(-.625, endRectangle.calulateX1(origin));
+        }
+
+        [TestMethod]
+        public void TestX2Calculation()
+        {
+            double width = endRectangle._width;
+            double origin = 0.0;
+            double clearance = 10;
+            double endMill = .125;
+
+            Assert.AreEqual(10.125, endRectangle.calulateX2(width, origin, clearance, endMill));
+        }
+
+        [TestMethod]
+        public void TestY1Calculation()
+        {
+            double origin = 0.0;
+
+            Assert.AreEqual(-.625, endRectangle.calulateY1(origin));
+        }
+
+        [TestMethod]
+        public void TestY2Calculation()
+        {
+            double origin = 0.0;
+
+            Assert.AreEqual(-.625, endRectangle.calulateY2(origin));
+        }
+
+        [TestMethod]
+        public void TestFirstRectangleCreation()
+        {
+        }
+
+        [TestMethod]
+        public void TestSecondtRectangleCreation()
+        {
+        }
+    }
+}
